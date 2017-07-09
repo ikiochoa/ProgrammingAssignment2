8600d040fd78220a89dae876f7a84cabcc7af171
ikiochoa <iki.ochoa@gmail.com>
2017-07-09 16:17:04 +0200

    Added cachematrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix: This function creates a special "matrix" object 
+##that can cache its inverse.
 
-## Write a short comment describing this function
+# makeCacheMatrix(mat)$set(mat)
+# makeCacheMatrix(mat)$get()
+# makeCacheMatrix(mat)$setinverse(solve(mat))
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        #inv = matrix()
+        set <- function(y = matrix()) {
+                x <<- y
+                inv <<- matrix()
+        }
+        get <- function() x
+        setinverse <- function(inverse = matrix()){  
+                inv <<- inverse
+                print(inv)
+        }
+        getinverse <- function() inv
+        list(set=set, get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
+# cacheSolve: This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already been 
+## calculated (and the matrix has not changed), then the cachesolve should 
+## retrieve the inverse from the cache.
+# Computing the inverse of a square matrix can be done with the solve 
+## function in R. For example, if X is a square invertible matrix, then 
+## solve(X) returns its inverse.
 
-## Write a short comment describing this function
+# cacheSolve(makeCacheMatrix(mat))
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
+}
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
